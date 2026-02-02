665b5026e724f078737c587cfe6d9ec4d7bcd93d
KishanJudge <kishanjudge@hotmail.com>
2026-02-02 19:34:22 +0000

    step 3

--- base.java
+++ base.java
@@ -5,21 +5,26 @@ public class base {
     
     public static void main(String[] args) {
         int bitstringSize = 8; // length 𝑛
-        String[] population = createPopulation(bitstringSize);
+        final int populationSize = 100;
+        String[] population = createPopulation(bitstringSize, populationSize);
 
-        for (int x = 0; x < 100; x++) {
-            //System.out.println(population[x]);
-            getScore(population[x]);
-        }
+        /*for (int x = 0; x < 100; x++) {
+            System.out.println(population[x]);
+            //getScore(population[x]);
+        }*/
 
-       
+        String[] newPopulation = crossover(population, populationSize, bitstringSize);
+
+        /*for (int x = 0; x < 100; x++) {
+            System.out.println(newPopulation[x]);
+            //getScore(newPopulation[x]);
+        }*/
         
     }
 
-    public static String[] createPopulation(int bitstringSize) {
+    public static String[] createPopulation(int bitstringSize, int populationSize) {
         //create a list of a hundred randomly generated bitstrings of length 𝑛 called population
         Random random = new Random();
-        int populationSize = 100;
         int populationCheck = 0;
         String[] population = new String[populationSize];
         do{
@@ -49,6 +54,24 @@ public class base {
         }
         return score;
     }
+
+    public static String[] crossover(String[] population, int populationSize, int bitstringSize) {
+        String[] newPopulation = new String[populationSize];
+        for (int i = 0; i < populationSize / 2; i++) {
+            String parent1 = population[2 * i];
+            String parent2 = population[(2 * i) +1];
+
+            String offspring1 = parent1.substring(0 , bitstringSize / 2)
+                    + parent2.substring(bitstringSize / 2 , bitstringSize);
+            String offspring2 = parent2.substring(0 , bitstringSize / 2)
+                    + parent1.substring(bitstringSize / 2 , bitstringSize);
+
+            newPopulation[2 * i] = offspring1;
+            newPopulation[(2 * i) + 1] = offspring2;
+
+
+        }
+        return newPopulation;
+    }
 }
 
-//fortnite
